f78e23870f13bbe90485558c9d9e845d399a5988
Teffandi <13221084@std.stei.itb.ac.id>
2024-01-14 20:55:28 +0700

    added CLA module

---
+++ work/@c@l@a/_primary.vhd
@@ -0,0 +1,11 @@
+library verilog;
+use verilog.vl_types.all;
+entity CLA is
+    port(
+        ADD_1           : in     vl_logic_vector(15 downto 0);
+        ADD_2           : in     vl_logic_vector(15 downto 0);
+        C               : in     vl_logic;
+        S               : out    vl_logic_vector(15 downto 0);
+        C_OUT           : out    vl_logic
+    );
+end CLA;
